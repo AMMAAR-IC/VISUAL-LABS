c26c075c8b71555cc9384fe0274e4ad9a8393807
AMMAAR BAKSHI <the.ammaar.bakshi@gmail.com>
2025-06-22 22:12:10 +0530

    Array 1.0

---
+++ NP/Array1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arrays = np.array([1,2,3,4,5,6])
+a = np.zeros(10, dtype=int)
+print(a)
+b = np.ones(10,dtype=int)
+print(b)
+c = np.arange(1,10,1)
+print(c)
+print(arrays[0])
+d = np.linspace(10,11,5)
+print(d)
+
+
+#TASKS
+#image flipping
+#random
+    #sync the ship
+    #guess the number
